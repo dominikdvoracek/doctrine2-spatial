24d8fccd1a032469d9c49b8149533931faaaf59e
Dominik Dvořáček <domi.dvoracek@gmail.com>
2016-05-31 17:50:10 +0200

    Fix type names

--- lib/CrEOF/Spatial/DBAL/Types/GeometryType.php
+++ lib/CrEOF/Spatial/DBAL/Types/GeometryType.php
@@ -29,7 +29,7 @@ namespace CrEOF\Spatial\DBAL\Types;
  * @author  Derek J. Lambert <dlambert@dereklambert.com>
  * @license http://dlambert.mit-license.org MIT
  */
-class GeometryType extends AbstractSpatialType
+abstract class GeometryType extends AbstractSpatialType
 {
 
 }
